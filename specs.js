0e89ac1974207db400e182d0120ec9659b27391b
Thaddeus Hughes <hughes.thad@gmail.com>
2020-03-22 10:21:41 -0400

    integrations, changed compute behavior to on enter, breaking out specs into its own doc for future use

---
+++ specs.js
@@ -0,0 +1,79 @@
+var motor_data = {
+		"neo": {
+			"max_rpm": 5880,
+			"free_current": 1.3,
+			"max_power": 516,
+			"stall_torque": 3.36,
+			"stall_current": 166
+		},"neo550": {
+			"max_rpm": 11000,
+			"free_current": 1.4,
+			"max_power": 279,
+			"stall_torque": 0.97,
+			"stall_current": 100
+		},
+
+		"cim": {
+			"max_rpm": 5330,
+			"free_current": 2.7,
+			"max_power": 337,
+			"stall_torque": 2.41,
+			"stall_current": 131
+		},
+
+		"falcon500": {
+			"max_rpm": 6380,
+			"free_current": 1.5,
+			"max_power": 783,
+			"stall_torque": 4.69,
+			"stall_current": 257
+		},
+
+		"minicim": {
+			"max_rpm": 5840,
+			"free_current": 3,
+			"max_power": 215,
+			"stall_torque": 1.41,
+			"stall_current": 89
+		},
+
+		"bag": {
+			"max_rpm": 13180,
+			"free_current": 1.8,
+			"max_power": 149,
+			"stall_torque": 0.43,
+			"stall_current": 53
+		},
+
+		"775pro": {
+			"max_rpm": 18730,
+			"free_current": 0.7,
+			"max_power": 347,
+			"stall_torque": 0.71,
+			"stall_current": 134
+		},
+
+		"am9015": {
+			"max_rpm": 14270,
+			"free_current": 3.7,
+			"max_power": 134,
+			"stall_torque": 0.36,
+			"stall_current": 71
+		},
+
+		"neverest": {
+			"max_rpm": 5480,
+			"free_current": 0.4,
+			"max_power": 25,
+			"stall_torque": 0.17,
+			"stall_current": 10
+		},
+
+		"rs550": {
+			"max_rpm": 19000,
+			"free_current": 0.4,
+			"max_power": 190,
+			"stall_torque": 0.38,
+			"stall_current": 84
+		}
+	}
