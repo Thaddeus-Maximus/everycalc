f4445eb053d541aa9fe6d470ba7786a5ad73d3e1
Thaddeus Hughes <hughes.thad@gmail.com>
2020-05-29 07:27:13 -0400

    starting electrical model, results seem sane at first, but theres nans somewhere

--- js/motors.js
+++ js/motors.js
@@ -30,43 +30,83 @@ function MOTOR_getSpec(motor, key){
 }
 
 function MOTOR_computeState(motor, from, value) {
-	// everything must be in base units here
 	let state = {};
-	let stall_torque = motor.count*motor.stall_torque*motor.efficiency*motor.pct_power;
-	let max_speed = motor.max_speed*motor.pct_power;
-	let stall_current = motor.count*motor.stall_current*motor.pct_power;
-	let free_current  = motor.count*motor.free_current*motor.pct_power;
-	switch(from) {
-		case 'torque':
-			state.current = (stall_current-free_current)*value/stall_torque + free_current;
-			state.torque  = value;
-			state.speed   = max_speed - value*max_speed/stall_torque;
-			break;
-		case 'speed':
-			state.current = (stall_current-free_current)*(max_speed-value)/max_speed + free_current;
-			state.torque  = (stall_torque)*(max_speed-value)/max_speed;
-			state.speed   = value;
-			break;
-		case 'current':
-			state.current = value;
-			state.torque  = (value - free_current)*stall_torque/(stall_current-free_current);
-			state.speed   = max_speed - state.torque*max_speed/stall_torque;
-			break;
+
+	// everything must be in base units here
+	if (motor.battery_voltage != undefined) {
+		let stall_torque = motor.count*motor.stall_torque*motor.efficiency;
+		let max_speed = motor.max_speed;
+		let stall_current = motor.count*motor.stall_current;
+		let free_current  = motor.count*motor.free_current;
+		switch(from) {
+			case 'speed':
+				state.voltage = (motor.battery_voltage + motor.wire_resistance*(stall_current-free_current)*value/max_speed)/(1+motor.wire_resistance*stall_current/motor.voltage);
+				let vfrac   = state.voltage/motor.voltage;
+				let spdfrac = (max_speed*vfrac-value)/max_speed*vfrac;
+				state.current = vfrac*((stall_current-free_current)*spdfrac + free_current);
+				if (state.current > motor.current_limit) {
+					// solve forwards now
+					state.current = motor.current_limit;
+					state.voltage = (state.current + (stall_current-free_current)*value/max_speed)/stall_current*motor.voltage;
+					vfrac   = state.voltage/motor.voltage;
+					spdfrac = (max_speed*vfrac-value)/max_speed*vfrac;
+				}
+				state.torque  = vfrac*(stall_torque*spdfrac);
+				state.speed   = value;
+				break;
+			case 'torque':
+			case 'current':
+				state.voltage = NaN;
+				state.current = NaN;
+				state.torque  = NaN;
+				state.speed   = NaN;
+				break;
+		}
+	} else {
+		let stall_torque = motor.count*motor.stall_torque*motor.efficiency;
+		let max_speed = motor.max_speed;
+		let stall_current = motor.count*motor.stall_current;
+		let free_current  = motor.count*motor.free_current;
+		switch(from) {
+			case 'torque':
+				state.voltage = motor.voltage;
+				state.current = (stall_current-free_current)*value/stall_torque + free_current;
+				state.torque  = value;
+				state.speed   = max_speed - value*max_speed/stall_torque;
+				break;
+			case 'speed':
+				state.voltage = motor.voltage;
+				state.current = (stall_current-free_current)*(max_speed-value)/max_speed + free_current;
+				state.torque  = (stall_torque)*(max_speed-value)/max_speed;
+				state.speed   = value;
+				break;
+			case 'current':
+				state.voltage = motor.voltage;
+				state.current = value;
+				state.torque  = (value - free_current)*stall_torque/(stall_current-free_current);
+				state.speed   = max_speed - state.torque*max_speed/stall_torque;
+				break;
+		}
 	}
+
 	return state;
 }
 
+function MOTOR_computeElectricalState(motor, from, value) {
+
+}
+
 function MOTOR_computeIdleCurrent(motor, torque_target) {
-	let stall_torque = motor.count*motor.stall_torque*motor.efficiency*motor.pct_power;
-	let max_speed = motor.max_speed*motor.pct_power;
-	let stall_current = motor.count*motor.stall_current*motor.pct_power;
-	let free_current  = motor.count*motor.free_current*motor.pct_power;
+	let stall_torque = motor.count*motor.stall_torque*motor.efficiency;
+	let max_speed = motor.max_speed;
+	let stall_current = motor.count*motor.stall_current;
+	let free_current  = motor.count*motor.free_current;
 
 	let cmd_pct = torque_target / stall_torque;
 	return (stall_current*cmd_pct-free_current*cmd_pct)*torque_target/(stall_torque*cmd_pct) + free_current*cmd_pct;
 }
 
-let MOTOR_PROPS = ['max_speed', 'free_current', 'stall_torque', 'stall_current', 'count', 'pct_power', 'efficiency'];
+let MOTOR_PROPS = ['max_speed', 'free_current', 'stall_torque', 'stall_current', 'count', 'efficiency'];
 let SELECTABLE_MOTOR_PROPS = ['max_speed', 'free_current', 'stall_torque', 'stall_current'];
 
 function MOTOR_selectMotor(id, motorName) {
@@ -74,21 +114,31 @@ function MOTOR_selectMotor(id, motorName) {
 		motorName = document.getElementById(id+"_select").value;
 	else
 		document.getElementById(id+"_select").value = motorName;
+	let nomVoltsInp = document.getElementById(id+"_voltage");
 
 	if (motorName == 'custom') {
 		SELECTABLE_MOTOR_PROPS.forEach(function(a){
 			document.getElementById(id+"_"+a).readOnly = false;
 		});
+		if (nomVoltsInp) nomVoltsInp.readOnly = false;
 	} else if (motorName in MOTOR_specs) {
 		SELECTABLE_MOTOR_PROPS.forEach(function(a) {
 			document.getElementById(id+"_"+a).readOnly = true;
 			setV(id+"_"+a, MOTOR_getSpec(motorName, a));
 		});
+		if (nomVoltsInp) {
+			nomVoltsInp.readOnly = true;
+			nomVoltsInp.value = MOTOR_getSpec(motorName, 'voltage');
+		}
 	} else {
 		SELECTABLE_MOTOR_PROPS.forEach(function(a) {
 			document.getElementById(id+"_"+a).readOnly = true;
 			setV(id+"_"+a, '***');
 		});
+		if (nomVoltsInp) {
+			nomVoltsInp.readOnly = true;
+			nomVoltsInp.value = '***';
+		}
 	}
 }
 
@@ -101,6 +151,21 @@ function MOTOR_packMotor(id) {
 	/*motor.free_current  *= getV(id+'_count')*getV(id+'_pct_power');
 	motor.stall_current *= getV(id+'_count')*getV(id+'_pct_power');
 	motor.stall_torque  *= getV(id+'_count')*getV(id+'_pct_power');*/
+	return motor;
+}
+
+
+//let MOTOR_ELEC_PROPS = ['max_speed', 'free_current', 'stall_torque', 'stall_current', 'count', 'efficiency', 'current_limit', 'battery_voltage', 'voltage', 'wire_resistance'];
+
+function MOTOR_packElectricalMotor(id) {
+	let motor = {};
+	for (prop of MOTOR_PROPS) {
+		motor[prop] = getV(id+'_'+prop);
+	}
+	motor.current_limit   = getV(id+'_current_limit',   +Infinity);
+	motor.voltage         = getV(id+'_voltage',         1);
+	motor.battery_voltage = getV(id+'_battery_voltage', motor.voltage);
+	motor.wire_resistance = getV(id+'_wire_resistance',  0);
 	return motor;
 }
 
@@ -118,14 +183,16 @@ var MOTOR_specs = {
 		"free_current": 1.3,
 		"max_power": 516,
 		"stall_torque": 3.36,
-		"stall_current": 166
+		"stall_current": 166,
+		"voltage": 12
 	},"neo550": {
 		"plain_name": "REV NEO 550",
 		"max_speed": 11000,
 		"free_current": 1.4,
 		"max_power": 279,
 		"stall_torque": 0.97,
-		"stall_current": 100
+		"stall_current": 100,
+		"voltage": 12
 	},
 
 	"cim": {
@@ -134,7 +201,8 @@ var MOTOR_specs = {
 		"free_current": 2.7,
 		"max_power": 337,
 		"stall_torque": 2.41,
-		"stall_current": 131
+		"stall_current": 131,
+		"voltage": 12
 	},
 
 	"falcon500": {
@@ -143,7 +211,8 @@ var MOTOR_specs = {
 		"free_current": 1.5,
 		"max_power": 783,
 		"stall_torque": 4.69,
-		"stall_current": 257
+		"stall_current": 257,
+		"voltage": 12
 	},
 
 	"minicim": {
@@ -152,7 +221,8 @@ var MOTOR_specs = {
 		"free_current": 3,
 		"max_power": 215,
 		"stall_torque": 1.41,
-		"stall_current": 89
+		"stall_current": 89,
+		"voltage": 12
 	},
 
 	"bag": {
@@ -161,7 +231,8 @@ var MOTOR_specs = {
 		"free_current": 1.8,
 		"max_power": 149,
 		"stall_torque": 0.43,
-		"stall_current": 53
+		"stall_current": 53,
+		"voltage": 12
 	},
 
 	"775pro": {
@@ -170,7 +241,8 @@ var MOTOR_specs = {
 		"free_current": 0.7,
 		"max_power": 347,
 		"stall_torque": 0.71,
-		"stall_current": 134
+		"stall_current": 134,
+		"voltage": 12
 	},
 
 	"am9015": {
@@ -179,7 +251,8 @@ var MOTOR_specs = {
 		"free_current": 3.7,
 		"max_power": 134,
 		"stall_torque": 0.36,
-		"stall_current": 71
+		"stall_current": 71,
+		"voltage": 12
 	},
 
 	"neverest": {
@@ -188,7 +261,8 @@ var MOTOR_specs = {
 		"free_current": 0.4,
 		"max_power": 25,
 		"stall_torque": 0.17,
-		"stall_current": 10
+		"stall_current": 10,
+		"voltage": 12
 	},
 
 	"rs550": {
@@ -197,7 +271,8 @@ var MOTOR_specs = {
 		"free_current": 0.4,
 		"max_power": 190,
 		"stall_torque": 0.38,
-		"stall_current": 84
+		"stall_current": 84,
+		"voltage": 12
 	},
 
 	"nxt_nimh": {
@@ -206,7 +281,8 @@ var MOTOR_specs = {
 		"free_current": 60e-3, 
 		"max_power": 1.07,
 		"stall_torque": 28e-2,
-		"stall_current": 950e-3
+		"stall_current": 950e-3,
+		"voltage": 7.2
 		// from https://www.philohome.com/nxtmotor/nxtmotor.htm
 	},
 
@@ -216,7 +292,8 @@ var MOTOR_specs = {
 		"free_current": 60e-3, 
 		"max_power": 1.7,
 		"stall_torque": 37e-2,
-		"stall_current": 1350e-3
+		"stall_current": 1350e-3,
+		"voltage": 9.0
 		// from https://www.philohome.com/nxtmotor/nxtmotor.htm
 	}
 }
